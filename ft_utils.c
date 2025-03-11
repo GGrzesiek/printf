97408bcad54e1418d24d1034526733b0056180c8
Grzegorz Kryszczuk <gkryszcz@c4r1s2.42warsaw.pl>
2025-03-11 10:38:54 +0100

    deleted unnecessary function

--- ft_utils.c
+++ ft_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_utils.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ggrzesiek <ggrzesiek@student.42.fr>        +#+  +:+       +#+        */
+/*   By: gkryszcz <gkryszcz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 11:14:20 by gkryszcz          #+#    #+#             */
-/*   Updated: 2025/03/03 18:26:37 by ggrzesiek        ###   ########.fr       */
+/*   Updated: 2025/03/11 10:37:46 by gkryszcz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,21 +27,6 @@ void	ft_putstr_fd(char *s, int fd)
 void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
-}
-
-void	*ft_calloc(size_t nmemb, size_t size)
-{
-	void	*data;
-
-	if (!nmemb || !size)
-		return (malloc(0));
-	if (nmemb && (nmemb * size) / nmemb != size)
-		return (NULL);
-	data = (void *)malloc(nmemb * size);
-	if (!data)
-		return (NULL);
-	ft_bzero(data, nmemb * size);
-	return (data);
 }
 
 void	ft_bzero(void *s, size_t n)
